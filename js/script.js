05a926df55fd3d5539169ef2aa1c79d179c8646a
Kaio Felipe Silva <kaiofelipe.js@gmail.com>
2020-01-13 21:51:08 -0300

    create animation in nav bar

--- js/script.js
+++ js/script.js
@@ -1,3 +1,28 @@
-import initMenuMobile from "./modules/menuMobile.js";
+window.onscroll = function() {
+  scrollFunction();
+};
 
-initMenuMobile();
+const menuLogo = document.querySelector(".menu-logo");
+const menu = document.querySelector(".menu");
+const menuFixed = document.querySelector(".menu-fixed");
+const menuList = document.querySelector(".menu-list");
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 100 ||
+    document.documentElement.scrollTop > 100
+  ) {
+    menuLogo.style.backgroundImage = "url('../assets/icons/logotipo.png')";
+    menuLogo.style.width = "90px";
+    menuLogo.style.height = "90px";
+    menu.style.top = "25px";
+    menuList.style.top = "250%";
+  } else {
+    menuLogo.style.backgroundImage =
+      "url('../assets/icons/logo_horizontal.png')";
+    menuLogo.style.width = "297px";
+    menuLogo.style.height = "125px";
+    menu.style.top = "40px";
+    menuList.style.top = "200%";
+  }
+}
