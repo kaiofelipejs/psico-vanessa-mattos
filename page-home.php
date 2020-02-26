028b17fa3863169018abbac006d52781565a6a21
Kaio Felipe Silva <kaiofelipe.js@gmail.com>
2020-02-26 16:36:58 -0300

    add reading time

--- page-home.php
+++ page-home.php
@@ -82,7 +82,13 @@
             </div>
             <div class="blog-post-title">
               <h3><?php the_title()?></h3>
-              <small><?php echo get_the_date('j \d\e F \d\e Y');?></small>
+              <div class="blog-post-data">
+                <svg id="clock" enable-background="new 0 0 443.294 443.294" viewBox="0 0 443.294 443.294"  xmlns="http://www.w3.org/2000/svg">
+                  <path d="m221.647 0c-122.214 0-221.647 99.433-221.647 221.647s99.433 221.647 221.647 221.647 221.647-99.433 221.647-221.647-99.433-221.647-221.647-221.647zm0 415.588c-106.941 0-193.941-87-193.941-193.941s87-193.941 193.941-193.941 193.941 87 193.941 193.941-87 193.941-193.941 193.941z"/>
+                  <path d="m235.5 83.118h-27.706v144.265l87.176 87.176 19.589-19.589-79.059-79.059z"/>
+                </svg>
+                <small><?php echo do_shortcode('[rt_reading_time postfix="minutos" postfix_singular="minuto"]') ?> • <?php echo get_the_date('j M, Y');?> </small>
+              </div>
             </div>
           </a>
             <div class="blog-post-info">
